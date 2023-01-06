8e95910e8a4fbda3de59f3ad62ffd2f029ba5622
MarcelloMartins <marcello08martins@outlook.com>
2023-01-05 21:55:56 -0300

    feat(header): created html and scss top header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./styles.module.scss";
+import Logo from "../../assets/logoM3.png";
+import Lupa from "../../assets/lupa.svg";
+import Cart from "../../assets/cart.svg";
+
+export default function Header() {
+  return (
+    <>
+      <header className={styles.Header}>
+        <div className={styles.firstDiv}>
+          <img className={styles.logo} src={Logo} alt="logo m3" />
+          <div className={styles.searchDiv}>
+            <input type="text" placeholder="Buscar..."></input>
+            <img src={Lupa} alt="lupa" />
+          </div>
+          <div className={styles.headerButton}>
+            <button className={styles.buttonEntrar}>Entrar</button>
+            <button className={styles.cartButton}>
+              <img src={Cart} alt="carrinho de compra" />
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
