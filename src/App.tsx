971a2650724b873293d5748017a53b10f94a9168
MarcelloMartins <marcello08martins@outlook.com>
2023-01-11 17:21:06 -0300

    fix(header): fix input scss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Institutional from "./pages/Institutional";
 
@@ -22,7 +21,6 @@ function App() {
     <div className="App">
       <Header />
       <RouterProvider router={router} />
-      <Footer />
     </div>
   );
 }
